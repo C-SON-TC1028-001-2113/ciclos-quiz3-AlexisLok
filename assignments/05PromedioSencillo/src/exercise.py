666202ae580885443066fb745712d27980f6cb67
AlexisLok <a01251886@itesm.mx>
2021-09-03 16:30:03 +0000

    Ejercicio1 de Quiz 3

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input(">>>"))
+    i=0
+    con = 0
+    while i < n:
+        x = float(input(">>>"))
+        con = con + x
+        i += 1
+    prom = con / n
+    print (prom)
 
 if __name__=='__main__':
     main()
